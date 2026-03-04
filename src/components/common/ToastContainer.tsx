e4d691814b47e7dc1c60ab329e95cf851d721120
dipta-sdd <sankarsandipta@gmail.com>
2026-03-05 04:17:52 +0600

    toast  moved to bottom

--- src/components/common/ToastContainer.tsx
+++ src/components/common/ToastContainer.tsx
@@ -8,7 +8,7 @@ export const ToastContainer: FC = () => {
   return (
     <div
       id="kkkkkkk"
-      className="tubebay-fixed tubebay-top-[10px] tubebay-right-[10px] tubebay-z-[999999] tubebay-flex tubebay-flex-col tubebay-gap-[10px] tubebay-min-w-[200px]"
+      className="tubebay-fixed tubebay-bottom-[20px] tubebay-right-[20px] tubebay-z-[999999] tubebay-flex tubebay-flex-col-reverse tubebay-gap-[10px] tubebay-min-w-[300px]"
     >
       {toasts.map((toast) => (
         <Toast key={toast.id} toast={toast} onDismiss={removeToast} />
